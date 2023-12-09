14c4dad2d81250a4dfad7100eeda56d8c6608761
Alexander Ribeiro <alexander2005rj@gmail.com>
2023-12-08 23:19:08 -0300

    chore: challenge finished

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,56 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+class Usuario(val nome: String, val email: String) 
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, val duracao: Int)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val nivel: Nivel, val conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(vararg usuarios: Usuario) {
+        for (usuario in usuarios) {
+            inscritos.add(usuario)
+       		println("O aluno ${usuario.nome} foi matriculado na formação $nome.")
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
+    // Criando alunos
+    val aluno1 = Usuario("John", "john@doe.com")
+    val aluno2 = Usuario("Jane", "jane@doe.com")
+    val aluno3 = Usuario("Dick Jones", "dick-jones@ocp.com")
+    val aluno4 = Usuario("Esteban", "estaban@maroto.com")
+    val aluno5 = Usuario("Minka", "minka@barbie.com")
+    
+    // Criando Conteudo Educacional
+    val ConteudoEducacional1 = ConteudoEducacional("Algoritmos", 100)
+    val ConteudoEducacional2 = ConteudoEducacional("Banco de dados", 120)
+    val ConteudoEducacional3 = ConteudoEducacional("Metologias ágeis", 70)
+    val ConteudoEducacional4 = ConteudoEducacional("Git e Github", 20)
+    val ConteudoEducacional5 = ConteudoEducacional("Backend com Kotlin", 60)
+    
+   // Criando Formações
+   val formacao1 = Formacao("MySQL", Nivel.BASICO, listOf(ConteudoEducacional2, ConteudoEducacional3))
+   val formacao2 = Formacao("Introdução a Programação", Nivel.INTERMEDIARIO, listOf(ConteudoEducacional1))
+   val formacao3 = Formacao("Formação Kotlin", Nivel.AVANCADO, listOf(ConteudoEducacional1, 
+                                                                      ConteudoEducacional2,
+                                                                      ConteudoEducacional3,
+                                                                      ConteudoEducacional4,
+                                                                      ConteudoEducacional5))
+   
+   // Matriculando Alunos nas Formações disponíveis
+   formacao1.matricular(aluno3, aluno4, aluno5)
+   formacao2.matricular(aluno4)
+   formacao3.matricular(aluno1, aluno2, aluno3, aluno4, aluno5)
+   
+   // Alunos por Formação
+   println("Inscritos na formação ${formacao1.nome}: ${formacao1.inscritos.joinToString(", ") { it.nome }}")
+   println("Inscritos na formação ${formacao2.nome}: ${formacao2.inscritos.joinToString(", ") { it.nome }}")
+   println("Inscritos na formação ${formacao3.nome}: ${formacao3.inscritos.joinToString(", ") { it.nome }}")
+   
 }
